ef29e263befa2224b34e0c4aab1c172032210882
Andrew Thomson <andrewguy9@Andrews-MacBook-Pro.local>
2013-04-23 08:53:33 -0700

    Add test_al function.

--- s3_lib.py
+++ s3_lib.py
@@ -124,9 +124,20 @@ def get_string_to_sign(method, content_md5, content_type, http_date, amz_headers
 # Testing #
 ###########
 
+def test_all():
+    print "GET TEST"
+    test_sign_get()
+    print "PUT TEST"
+    test_sign_put()
+    print "LIST TEST"
+    test_sign_list()
+    print "COPY TEST"
+    test_sign_copy()
+
 def validate_signature(string, expected_string, expected_signature):
-    print "testing..."
+    print "output:"
     print "***\n" + string + "\n***\n"
+    print "expected:"
     print "---\n" + expected_string + "\n---\n"
     assert(string == expected_string)
     secret = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
@@ -149,13 +160,13 @@ def test_sign_put():
     expected_signature = "MyyxeRY7whkBe+bq8fHCL/2kKUg="
     validate_signature(string, expected_string, expected_signature)
 
-def test_list():
+def test_sign_list():
     string = get_string_to_sign("GET","", "", "Tue, 27 Mar 2007 19:42:41 +0000", {}, "/johnsmith/")
     expected_string = "GET\n\n\nTue, 27 Mar 2007 19:42:41 +0000\n/johnsmith/"
     expected_signature = "htDYFYduRNen8P9ZfE/s9SuKy0U="
     validate_signature(string, expected_string, expected_signature)
 
-def test_copy():
+def test_sign_copy():
     string = get_string_to_sign("PUT", "", "", "Wed, 20 Feb 2008 22:12:21 +0000", {"x-amz-copy-source":"/pacific/flotsam"}, "/atlantic/jetsam")
     expected_string = "PUT\n\n\nWed, 20 Feb 2008 22:12:21 +0000\nx-amz-copy-source:/pacific/flotsam\n/atlantic/jetsam"
     expected_signature = "ENoSbxYByFA0UGLZUqJN5EUnLDg="
