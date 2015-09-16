195fd34d303ff5ca7ed1f7586d08403ab7ba6b7a
Andrew Thomson <athomsonguy@gmail.com>
2015-09-15 18:24:41 -0700

    add s3sign.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Andrew Thomson',
     author_email='athomsonguy@gmail.com',
     packages=['s3lib', 's3lib.test'],
-    scripts=['bin/s3get', 'bin/s3cp', 'bin/s3head', 'bin/s3rm', 'bin/s3ls', 'bin/s3put'],
+    scripts=['bin/s3get', 'bin/s3cp', 'bin/s3head', 'bin/s3rm', 'bin/s3ls', 'bin/s3put', 'bin/s3sign'],
     url='http://pypi.python.org/pypi/S3Lib/',
     license='LICENSE.txt',
     description='Library and utilities for interfacing wtih S3',
