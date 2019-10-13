42340852cf68071b5bfc76370249afdb821f5545
Andrew Thomson <athomsonguy@gmail.com>
2019-10-12 23:41:13 -0700

    get_string_to_sign returns utf-8 encoded bytes.

--- s3lib/test__init__.py
+++ s3lib/test__init__.py
@@ -9,25 +9,25 @@ def validate_signature(string, expected_string, expected_signature):
 
 def test_sign_get():
   string = get_string_to_sign("GET","", "", "Tue, 27 Mar 2007 19:36:42 +0000", {}, "/johnsmith/photos/puppy.jpg")
-  expected_string = "GET\n\n\nTue, 27 Mar 2007 19:36:42 +0000\n/johnsmith/photos/puppy.jpg"
+  expected_string = "GET\n\n\nTue, 27 Mar 2007 19:36:42 +0000\n/johnsmith/photos/puppy.jpg".encode('utf-8')
   expected_signature = b"bWq2s1WEIj+Ydj0vQ697zp+IXMU="
   validate_signature(string, expected_string, expected_signature)
 
 def test_sign_put():
   string = get_string_to_sign("PUT", "", "image/jpeg", "Tue, 27 Mar 2007 21:15:45 +0000", {}, "/johnsmith/photos/puppy.jpg" )
-  expected_string = "PUT\n\nimage/jpeg\nTue, 27 Mar 2007 21:15:45 +0000\n/johnsmith/photos/puppy.jpg"
+  expected_string = "PUT\n\nimage/jpeg\nTue, 27 Mar 2007 21:15:45 +0000\n/johnsmith/photos/puppy.jpg".encode('utf-8')
   expected_signature = b"MyyxeRY7whkBe+bq8fHCL/2kKUg="
   validate_signature(string, expected_string, expected_signature)
 
 def test_sign_list():
   string = get_string_to_sign("GET","", "", "Tue, 27 Mar 2007 19:42:41 +0000", {}, "/johnsmith/")
-  expected_string = "GET\n\n\nTue, 27 Mar 2007 19:42:41 +0000\n/johnsmith/"
+  expected_string = "GET\n\n\nTue, 27 Mar 2007 19:42:41 +0000\n/johnsmith/".encode('utf-8')
   expected_signature = b"htDYFYduRNen8P9ZfE/s9SuKy0U="
   validate_signature(string, expected_string, expected_signature)
 
 @pytest.mark.skip()
 def test_sign_copy():
   string = get_string_to_sign("PUT", "", "", "Wed, 20 Feb 2008 22:12:21 +0000", {"x-amz-copy-source":"/pacific/flotsam"}, "/atlantic/jetsam")
-  expected_string = "PUT\n\n\nWed, 20 Feb 2008 22:12:21 +0000\nx-amz-copy-source:/pacific/flotsam\n/atlantic/jetsam"
+  expected_string = "PUT\n\n\nWed, 20 Feb 2008 22:12:21 +0000\nx-amz-copy-source:/pacific/flotsam\n/atlantic/jetsam".encode('utf-8')
   expected_signature = b"ENoSbxYByFA0UGLZUqJN5EUnLDg="
   validate_signature(string, expected_string, expected_signature)
