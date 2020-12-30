a02d16474d2f6156f231c31ceb75cf208cb8bbf8
Andrew Thomson <athomsonguy@gmail.com>
2020-12-29 17:40:50 -0800

    fix tox.ini tests, missing requirement on docopt

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ test_requires = ['tox', 'pytest']
 
 setup(
     name='S3Lib',
-    version='1.2.0',
+    version='1.2.1',
     author='Andrew Thomson',
     author_email='athomsonguy@gmail.com',
     packages=['s3lib'],
