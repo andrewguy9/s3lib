01137d10be78420d86f516c9b7f00a7125e3075d
Andrew Thomson <athomsonguy@gmail.com>
2022-12-03 23:13:18 -0800

    pypi says we are on 1.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ test_requires = ['tox', 'pytest']
 
 setup(
     name='S3Lib',
-    version='1.4.0',
+    version='1.5.0',
     author='Andrew Thomson',
     author_email='athomsonguy@gmail.com',
     packages=['s3lib'],
