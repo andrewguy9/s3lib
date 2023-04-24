a4bf0fc9bf4b430aa6997febf28d62baf783b959
Andrew Thomson <athomsonguy@gmail.com>
2023-04-24 13:58:54 -0700

    Add decorator for rate limiting function calls. Fix lint errors.

--- s3lib/utils.py
+++ s3lib/utils.py
@@ -1,63 +1,108 @@
+from functools import wraps
+from time import perf_counter, sleep
+
 ############################
 # Python Iteration Helpers #
 ############################
 
+
 def take(size, collection):
-  if size < 1:
-    raise ValueError("Size must be 1 or greater")
-  for _, elem in zip(range(size), collection):
-    yield elem
+    if size < 1:
+        raise ValueError("Size must be 1 or greater")
+    for _, elem in zip(range(size), collection):
+        yield elem
+
 
 def batchify(size, collection):
-  if size < 1:
-    raise ValueError("Size must be 1 or greater")
-  iterator = iter(collection)
-  while True:
-    batch = list(take(size, iterator))
-    if len(batch) == 0:
-      break
-    else:
-      yield batch
+    if size < 1:
+        raise ValueError("Size must be 1 or greater")
+    iterator = iter(collection)
+    while True:
+        batch = list(take(size, iterator))
+        if len(batch) == 0:
+            break
+        else:
+            yield batch
 
 ##########################
 # Signing Util Functions #
 ##########################
 
+
 def split_headers(headers):
-  """Some headers are special to amazon. Splits those from regular http headers"""
-  amz_headers = {}
-  reg_headers = {}
-  for cur in headers:
-    if cur.lower().startswith('x-amz-'):
-      amz_headers[cur] = headers[cur]
-    else:
-      reg_headers[cur] = headers[cur]
-  return (amz_headers, reg_headers)
+    """
+    Some headers are special to amazon. Splits those from regular http headers.
+    """
+    amz_headers = {}
+    reg_headers = {}
+    for cur in headers:
+        if cur.lower().startswith('x-amz-'):
+            amz_headers[cur] = headers[cur]
+        else:
+            reg_headers[cur] = headers[cur]
+    return (amz_headers, reg_headers)
 
-subresources = ["versioning", "location", "acl", "torrent", "lifecycle", "versionid", "delete"]
+
+subresources = ["versioning",
+                "location",
+                "acl",
+                "torrent",
+                "lifecycle",
+                "versionid",
+                "delete",
+                ]
+
+
 def split_args(args):
-  return {subresource:args[subresource]
-          for subresource in subresources if subresource in args}
+    return {subresource: args[subresource]
+            for subresource in subresources if subresource in args}
 
-def get_string_to_sign(method, content_md5, content_type, http_date, amz_headers, resource):
-  """
-  method is str.
-  content_md5 is ?
-  content_type is str
-  http_date is str
-  amz_headers is dict string string
-  resource is ?
-  returns utf-8 encoded bytes.
-  """
-  key_header_strs = [ (name.lower(), "%s:%s" % (name.lower(), amz_headers[name])) for name in list(amz_headers.keys()) ]
-  header_list = [x[1] for x in sorted(key_header_strs)]
-  header_str = "\n".join(header_list)
-  if header_str:
-    header_str+="\n"
-  string = "%s\n%s\n%s\n%s\n%s%s" % (method, content_md5, content_type, http_date, header_str, resource, )
-  return string.encode('utf-8')
+
+def get_string_to_sign(method, content_md5, content_type,
+                       http_date, amz_headers, resource):
+    """
+    method is str.
+    content_md5 is ?
+    content_type is str
+    http_date is str
+    amz_headers is dict string string
+    resource is ?
+    returns utf-8 encoded bytes.
+    """
+    key_header_strs = [(name.lower(), f"{name.lower()}:{amz_headers[name]}")
+                       for name in list(amz_headers.keys())]
+    header_list = [x[1] for x in sorted(key_header_strs)]
+    header_str = "\n".join(header_list)
+    if header_str:
+        header_str += "\n"
+    string = "%s\n%s\n%s\n%s\n%s%s" % \
+        (method, content_md5, content_type, http_date, header_str, resource, )
+    return string.encode('utf-8')
+
 
 def raise_http_resp_error(resp):
-    message = "S3 request failed with:\n%s %s\n%s\n%s" % (resp.status, resp.reason, resp.msg, resp.read())
+    message = "S3 request failed with:\n%s %s\n%s\n%s" % \
+        (resp.status, resp.reason, resp.msg, resp.read())
     raise ValueError(message)
 
+
+def rate_limited(max_per_second, time_keeper=perf_counter, waiter=sleep):
+    """
+    Decorator function to rate limit the function calls
+    """
+    min_interval = 1.0 / float(max_per_second)
+
+    def decorate(func):
+        @wraps(func)
+        def rate_limited_function(*args, **kargs):
+            start = time_keeper()
+            elapsed = start - rate_limited_function.last_time_called
+            left_to_wait = min_interval - elapsed
+            if left_to_wait > 0.0:
+                waiter(left_to_wait)
+            ret = func(*args, **kargs)
+            rate_limited_function.last_time_called = start
+            return ret
+        rate_limited_function.last_time_called = time_keeper() - min_interval
+        return rate_limited_function
+    return decorate
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,21 +1,66 @@
-from s3lib.utils import *
+from s3lib.utils import batchify, rate_limited, split_args, take
+from unittest.mock import MagicMock
+import pytest
+
 
 def test_take():
-  assert(list(take(3, [])) == [])
-  assert(list(take(3, [1,2])) == [1,2])
-  assert(list(take(3, [1,2, 3, 4])) == [1,2,3])
-  i = iter(list(range(7)))
-  assert(list(take(3, i)) == [0,1,2])
-  assert(list(take(3, i)) == [3,4,5])
-  assert(list(take(3, i)) == [6])
+    assert list(take(3, [])) == []
+    assert list(take(3, [1, 2])) == [1, 2]
+    assert list(take(3, [1, 2, 3, 4])) == [1, 2, 3]
+    i = iter(list(range(7)))
+    assert list(take(3, i)) == [0, 1, 2]
+    assert list(take(3, i)) == [3, 4, 5]
+    assert list(take(3, i)) == [6]
+
 
 def test_batchify():
-  assert(list(batchify(3, [])) == [])
-  assert(list(batchify(3, [1])) == [[1]])
-  assert(list(batchify(3, [1,2,3])) == [[1,2,3]])
-  assert(list(batchify(3, [1,2,3,4])) == [[1,2,3],[4]])
-  assert(list(batchify(3, iter([1,2,3,4]))) == [[1,2,3],[4]])
+    assert list(batchify(3, [])) == []
+    assert list(batchify(3, [1])) == [[1]]
+    assert list(batchify(3, [1, 2, 3])) == [[1, 2, 3]]
+    assert list(batchify(3, [1, 2, 3, 4])) == [[1, 2, 3], [4]]
+    assert list(batchify(3, iter([1, 2, 3, 4]))) == [[1, 2, 3], [4]]
+
 
 def test_split_args():
-  assert split_args({"delete":None}) == {"delete": None}
-  assert split_args({"delete":None, 'a':'b'}) == {"delete": None}
+    assert split_args({"delete": None}) == {"delete": None}
+    assert split_args({"delete": None, 'a': 'b'}) == {"delete": None}
+
+
+@pytest.mark.parametrize(
+    "rate,calls,latency,expected_clock, expected_sleep",
+    [
+        # Work slower than limit, no sleeps.
+        (2,  10, 1, 10, 0),
+        # Calls are balanced with time, so no sleeping.
+        (1,  10, 1, 10, 0),
+        # 1/2 the time we are early and sleep for 1 sec.
+        (1/2.0, 10, 1, 10+5, 5*1),
+    ],)
+def test_rate_limited(rate, calls, latency,
+                      expected_clock, expected_sleep):
+    sleeper = MagicMock(side_effect=lambda t: print("slept", t))
+    doer = MagicMock(side_effect=lambda: print("doing"))
+
+    def time_slept():
+        return sum(map(lambda call: call[0][0], sleeper.call_args_list))
+
+    def current_time():
+        now = doer.call_count * latency + time_slept()
+        print("now:", now)
+        return now
+
+    clock = MagicMock(side_effect=current_time)
+    limited = rate_limited(rate, clock, sleeper)(doer)
+    for _ in range(calls):
+        print("about to call", current_time(),
+              "last time", limited.last_time_called)
+        limited()
+
+    print(dir(sleeper))
+    print("slept", sleeper.call_count)
+    print("clock", clock.call_count)
+    print("doer", doer.call_count)
+    print("clock", sleeper.call_args_list)
+    print("time slept", time_slept())
+    assert clock() == expected_clock
+    assert time_slept() == expected_sleep
